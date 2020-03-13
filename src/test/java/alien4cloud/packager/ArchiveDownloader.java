230b20cc466fa8a18852e6f19ae98e31a25a03c2
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-03-13 10:49:17 +0100

    [ALIEN-3404] move alien-premium-storage-types & docker-types into alien4cloud-extended-types

--- src/test/java/alien4cloud/packager/ArchiveDownloader.java
+++ src/test/java/alien4cloud/packager/ArchiveDownloader.java
@@ -21,15 +21,14 @@ public class ArchiveDownloader {
         RepositoryManager.cloneOrCheckout(gitDirectory, "https://github.com/alien4cloud/alien4cloud-extended-types.git", Version.PROJECT_VERSION,
                 "alien4cloud-extended-types");
                 // docker types
-        RepositoryManager.cloneOrCheckout(gitDirectory, "https://github.com/alien4cloud/docker-tosca-types.git", Version.PROJECT_VERSION,
-                "docker-tosca-types");
+
         // Do we want to initialize alien with sample topology.
         // repositoryManager.cloneOrCheckout(gitDirectory, "https://github.com/alien4cloud/samples.git", "master", "samples");
 
         FileUtil.zip(gitDirectory.resolve("tosca-normative-types"), zipDirectory.resolve("tosca-normative-types.zip"));
         FileUtil.zip(gitDirectory.resolve("alien4cloud-extended-types/alien-base-types"), zipDirectory.resolve("alien-base-types.zip"));
         FileUtil.zip(gitDirectory.resolve("alien4cloud-extended-types/alien-extended-storage-types"), zipDirectory.resolve("alien-extended-storage-types.zip"));
-        FileUtil.zip(gitDirectory.resolve("docker-tosca-types/docker-types"), zipDirectory.resolve("docker-tosca-types.zip"));
+        FileUtil.zip(gitDirectory.resolve("alien4cloud-extended-types/docker-types"), zipDirectory.resolve("docker-tosca-types.zip"));
         // zip archives
         // FileUtil.zip(from, to);
     }
